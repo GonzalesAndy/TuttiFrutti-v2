e61e34009bd490377d50aeb10896b3e546147f31
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2024-02-13 09:20:41 +0100

    Corrections

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,12 +4,9 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use App\Service\DiscogsApiService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Webmozart\Assert\Tests\StaticAnalysis\string;
-use App\Controller\RandomFruitController;
 
 class HomeController extends AbstractController
 {
