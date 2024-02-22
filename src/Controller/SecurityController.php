d282e3e8720cba1968080e44fb8a132f89655e8c
LucasLeiner <lucasleiner05@gmail.com>
2024-02-22 10:28:40 +0100

    effet covers

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,10 +17,8 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
@@ -37,7 +35,6 @@ class SecurityController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
